e8af344974023d6110e86524751697bfc77ad89a
bsonghao <bsonghao@gmail.con>
2024-12-14 12:55:11 -0500

    implement the algorithm that bring an arbitary MPS into the canonical form

---
+++ MPS_canonical/MPS_canonical.py
@@ -0,0 +1,136 @@
+# system imports
+import io
+import time
+import os
+from os.path import abspath, join, dirname, basename
+import sys
+import cProfile
+import pstats
+import itertools as it
+
+
+# third party imports
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+import parse  # used for loading data files
+import pandas as pd
+import opt_einsum as oe
+
+class MPS_canonical(object):
+    """
+    The object implement the algorithm that bring an arbitary MPS into caonical form
+    """
+    def __init__(self, D, d, L):
+        """
+        initialize a random MPO and MPS
+        D: bond dimension of the MPS
+        d: physical dimension at each site
+        L: number of site
+        """
+        self.D = D
+        self.d = d
+        self.L = L
+
+        # initial the input MPS and the local operator as python dictionary
+        self.input_MPS = {}
+
+        # construct an arbitary MPS
+        for site in range(L):
+            # Determine dimensions of the current tensor
+            left_bond_dim = D if site > 0 else 1  # Left bond dimension is 1 for the first site
+            right_bond_dim = D if site < L - 1 else 1  # Right bond dimension is 1 for the last site
+
+            # Generate a random tensor of shape (left_bond_dim, phys_dim, right_bond_dim)
+            tensor = np.random.randn(left_bond_dim, d, right_bond_dim)
+
+            self.input_MPS[site] = tensor
+
+        # print input MPS for debug
+        for site in self.input_MPS.keys():
+            print("site:{:}".format(site+1))
+            print("MPS matrix shape:{:}".format(self.input_MPS[site].shape))
+            print("MPS matrix:\n{:}".format(self.input_MPS[site]))
+
+    def left_canonical(self):
+        """procedure to bring the MPS into left canonical form"""
+        def _local_caonical(input_tensor):
+            """produce left-canonical matrix at each site"""
+            left_bond_dim, phys_dim, right_bond_dim = input_tensor.shape
+            # reshape the input tensor into the shape (left_bond_dim * phys_dim, right_bond_dim)
+            input_tensor = input_tensor.reshape(left_bond_dim*phys_dim, right_bond_dim)
+            # SVD the reshaped tensor
+            A, S, Vh = np.linalg.svd(input_tensor, full_matrices=False)
+            # reshape the decomposed tensor in to the original shape
+            output_tensor = A.reshape(left_bond_dim, phys_dim, right_bond_dim)
+
+            return output_tensor, S, Vh
+
+        D, d, L = self.D, self.d, self.L
+        self.left_canonical_MPS = {}
+        # loop over each site of the MPS to form left-canonical MPS
+        for site in range(L):
+            # base case
+            if site == 0:
+                input_tensor = self.input_MPS[site]
+            else:
+                input_tensor = np.einsum('s,sa,aib->sib', S, V, self.input_MPS[site])
+
+            A, S, V = _local_caonical(input_tensor)
+
+            # site the decomponsed tensor at each site
+            self.left_canonical_MPS[site] = A
+
+        # check if the procedure produce the lelf-canonical MPS
+        print("Left canonical MPS:")
+        for site in range(L):
+            tensor = self.left_canonical_MPS[site]
+            left_bond_dim, phys_dim, right_bond_dim = tensor.shape
+            assert np.allclose(np.einsum('bia,bic->ac', tensor, tensor), np.eye(right_bond_dim))
+            print("Site {:}:".format(site+1))
+            print("shape:{:}".format(self.left_canonical_MPS[site].shape))
+            print("tensor:\n{:}".format(self.left_canonical_MPS[site]))
+
+        return
+
+    def right_canonical(self):
+        """procedure to bring the MPS into right-canonical form"""
+        def _local_caonical(input_tensor):
+            """produce left-canonical matrix at each site"""
+            left_bond_dim, phys_dim, right_bond_dim = input_tensor.shape
+            # reshape the input tensor into the shape (left_bond_dim * phys_dim, right_bond_dim)
+            input_tensor = input_tensor.reshape(left_bond_dim, right_bond_dim*phys_dim)
+            # SVD the reshaped tensor
+            U, S, B = np.linalg.svd(input_tensor, full_matrices=False)
+            # reshape the decomposed tensor in to the original shape
+            output_tensor = B.reshape(left_bond_dim, phys_dim, right_bond_dim)
+
+            return output_tensor, S, U
+
+        D, d, L = self.D, self.d, self.L
+        self.right_canonical_MPS = {}
+        # loop over each site of the MPS to form left-canonical MPS
+        for i in range(L):
+            site = L-i-1
+            # base case
+            if site == L-1:
+                input_tensor = self.input_MPS[site]
+            else:
+                input_tensor = np.einsum('aib,bs,s->ais', self.input_MPS[site], U, S)
+
+            B, S, U = _local_caonical(input_tensor)
+
+            # site the decomponsed tensor at each site
+            self.right_canonical_MPS[site] = B
+
+        # check if the procedure produce the lelf-canonical MPS
+        print("Right canonical MPS:")
+        for site in range(L):
+            tensor = self.right_canonical_MPS[site]
+            left_bond_dim, phys_dim, right_bond_dim = tensor.shape
+            assert np.allclose(np.einsum('aib,cib->ac', tensor, tensor), np.eye(left_bond_dim))
+            print("Site {:}:".format(site+1))
+            print("shape:{:}".format(self.right_canonical_MPS[site].shape))
+            print("tensor:\n{:}".format(self.right_canonical_MPS[site]))
+
+        return
---
+++ MPS_canonical/test.py
@@ -0,0 +1,34 @@
+
+import io
+import time
+import os
+from os.path import abspath, join, dirname, basename
+import sys
+import cProfile
+import pstats
+
+# third party import
+import numpy as np
+
+# import the path to the package
+project_dir = abspath(join(dirname(__file__), '/Users/pauliebao/Tensor_play_ground/MPS_canonical/'))
+sys.path.insert(0, project_dir)
+
+# local import
+from MPS_canonical import MPS_canonical
+
+def main():
+    """main function for MPS decomposition algorithm"""
+    # input tensor shape
+    bond_dimension = 3
+    num_state = 4
+    num_site = 5
+
+    tensor = MPS_canonical(bond_dimension, num_state, num_site)
+    tensor.left_canonical()
+    tensor.right_canonical()
+
+    return
+
+if (__name__ == '__main__'):
+    main()
