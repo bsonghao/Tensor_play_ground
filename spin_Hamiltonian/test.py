e9fb9655978041fc3926670f67410c92ea5b555d
bsonghao <bsonghao@gmail.con>
2025-04-13 16:16:55 -0400

    add test script for spin_Hamiltonian

---
+++ spin_Hamiltonian/test.py
@@ -0,0 +1,36 @@
+
+import io
+import time
+import os
+from os.path import abspath, join, dirname, basename
+import sys
+import cProfile
+import pstats
+
+# third party import
+import numpy as np
+
+# import the path to the package
+project_dir = abspath(join(dirname(__file__), '/Users/pauliebao/Tensor_play_ground/spin_Hamiltonian/'))
+sys.path.insert(0, project_dir)
+
+# local import
+from spin_Hamiltonian import spin_Hamiltonian
+
+def main():
+    """main function for MPS decomposition algorithm"""
+    # input tensor shape
+    L = 10
+    J = 2.
+    h = 1.
+    Jz = 1.
+
+
+    tensor = spin_Hamiltonian(L, J, Jz, h)
+    tensor.ground_state_search(num_sweep=10, D=2)
+
+
+    return
+
+if (__name__ == '__main__'):
+    main()
